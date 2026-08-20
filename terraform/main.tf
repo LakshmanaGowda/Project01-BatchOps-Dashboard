e0f4eba7ce422f61c8780875d37302b0da1dce50
LakshmanaGowda <lakshmanag1417@gmail.com>
2026-08-20 17:00:07 +0530

    Moved the tfstate file to s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "terraform-state-lakshmana-ap-south-1"
+    key    = "BatchOps-Dashboard/terraform.tfstate"
+    region = "ap-south-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
